7d169c04c700a19f996c1db935ccc4a9c8c47ea4
Sujay-Mohanty <sujaymohanty2001@gmail.com>
2025-05-08 07:26:27 +0530

    third commit for IMS

--- src/main/java/com/spring/ims/controller/ProductController.java
+++ src/main/java/com/spring/ims/controller/ProductController.java
@@ -1,5 +1,32 @@
 package com.spring.ims.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.ims.entity.Product;
+import com.spring.ims.repository.ProductRepository;
+
+@Controller
+@RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/addP")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "productAdd"; // This should match the HTML filename without extension
+    }
+
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute Product product) {
+        productRepository.save(product);
+        return "productAdd";
+    }
 }
--- src/main/java/com/spring/ims/entity/Product.java
+++ src/main/java/com/spring/ims/entity/Product.java
@@ -1,92 +1,42 @@
 package com.spring.ims.entity;
 
-//import org.hibernate.annotations.Table;
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //@Setter
 //@Getter
 //@ToString
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-@Table(name = "imsproducts")
+@Table(name="imsprod")
 public class Product {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	// MYSQL uses Identity , Sequence as the Generation Type
-	// For MYSQL need to use IDENTTIY as the generation type
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	//MYSQL uses Identity , Sequence as the Generation Type
+	//For MYSQL need to use IDENTTIY as the generation type
 	private long id;
 	private String name;
 	private String description;
 	private int quantity;
 	private double price;
-
-	public Product() {
-	}
-
-	public Product(long id, String name, String description, int quantity, double price) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.quantity = quantity;
-		this.price = price;
-	}
-
-	public Product(String name, String description, int quantity, double price) {
-		super();
-//		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.quantity = quantity;
-		this.price = price;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
+	
 }
+
+	
+
+	
+	
+	
+	
